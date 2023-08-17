e29120e0047be6aacadbd73433216b64689b09b2
Muskan <muskan@darukamuskan@gmail.com>
2023-08-17 18:04:57 +0530

    fix: formatting intendation

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -15,32 +15,32 @@ function Calculator() {
     const calculate = () => {
         setResult(eval(result).toString());
     }
-  return (
-  
-    <div className='calc'>
-        <form>
-        <input type="text" placeholder='0' id="answer" value={result}/>
-        <input type="button" value="9" className='button' onClick={clickHandler}/>
-        <input type="button" value="8" className='button' onClick={clickHandler}/>
-        <input type="button" value="7" className='button' onClick={clickHandler}/>
-        <input type="button" value="6" className='button' onClick={clickHandler}/>
-        <input type="button" value="5" className='button' onClick={clickHandler}/>
-        <input type="button" value="4" className='button' onClick={clickHandler}/>
-        <input type="button" value="3" className='button' onClick={clickHandler}/>
-        <input type="button" value="2" className='button' onClick={clickHandler}/>
-        <input type="button" value="1" className='button' onClick={clickHandler}/>
-        <input type="button" value="0" className='button' onClick={clickHandler}/>
-        <input type="button" value="." className='button' onClick={clickHandler}/>
-        <input type="button" value="+" className='button' onClick={clickHandler}/>
-        <input type="button" value="-" className='button' onClick={clickHandler}/>
-        <input type="button" value="*" className='button' onClick={clickHandler}/>
-        <input type="button" value="/" className='button' onClick={clickHandler}/>
-        <input type="button" value="%" className='button' onClick={clickHandler}/>
-        <input type="button" value="Clear" className='button1' onClick={clearDisplay}/>
-        <input type="button" value="=" className='button1' onClick={calculate}/>
-        </form>
-    </div>
-  )
+    return (
+
+        <div className='calc'>
+            <form>
+                <input type="text" placeholder='0' id="answer" value={result} />
+                <input type="button" value="9" className='button' onClick={clickHandler} />
+                <input type="button" value="8" className='button' onClick={clickHandler} />
+                <input type="button" value="7" className='button' onClick={clickHandler} />
+                <input type="button" value="6" className='button' onClick={clickHandler} />
+                <input type="button" value="5" className='button' onClick={clickHandler} />
+                <input type="button" value="4" className='button' onClick={clickHandler} />
+                <input type="button" value="3" className='button' onClick={clickHandler} />
+                <input type="button" value="2" className='button' onClick={clickHandler} />
+                <input type="button" value="1" className='button' onClick={clickHandler} />
+                <input type="button" value="0" className='button' onClick={clickHandler} />
+                <input type="button" value="." className='button' onClick={clickHandler} />
+                <input type="button" value="+" className='button' onClick={clickHandler} />
+                <input type="button" value="-" className='button' onClick={clickHandler} />
+                <input type="button" value="*" className='button' onClick={clickHandler} />
+                <input type="button" value="/" className='button' onClick={clickHandler} />
+                <input type="button" value="%" className='button' onClick={clickHandler} />
+                <input type="button" value="Clear" className='button1' onClick={clearDisplay} />
+                <input type="button" value="=" className='button1' onClick={calculate} />
+            </form>
+        </div>
+    )
 }
 
 export default Calculator
